d7f630e92b65751e4feac0bc845efbb4995504bc
Leonardo Holanda <leonardoholanda_0@hotmail.com>
2017-11-26 02:14:31 -0300

    Fix quest Long Live Lord Of Flame

--- L2JBrasil_DP/data/jscript/quests/4_LongLiveLordOfFlame/__init__.py
+++ L2JBrasil_DP/data/jscript/quests/4_LongLiveLordOfFlame/__init__.py
@@ -15,51 +15,41 @@ VARKEES          = 30566
 TATARU_ZU_HESTUI = 30585
 GANTAKI_ZU_URUTU = 30587
 
-#GIFTS
-HONEY_KHANDAR  = 1541
-BEAR_FUR_CLOAK = 1542
-BLOODY_AXE     = 1543
-ANCESTOR_SKULL = 1544
-SPIDER_DUST    = 1545
-DEEP_SEA_ORB   = 1546
-NPC_GIFTS = {TATARU_ZU_HESTUI:BEAR_FUR_CLOAK,VARKEES:HONEY_KHANDAR,GR00KIN:BLOODY_AXE,USKA:ANCESTOR_SKULL,KUNAI:SPIDER_DUST,GANTAKI_ZU_URUTU:DEEP_SEA_ORB}
+HONEY_KHANDAR,BEAR_FUR_CLOAK,BLOODY_AXE,ANCESTOR_SKULL,SPIDER_DUST,DEEP_SEA_ORB = range(1541,1547)
+NPC_GIFTS = {30585:BEAR_FUR_CLOAK,30566:HONEY_KHANDAR,30562:BLOODY_AXE,30560:ANCESTOR_SKULL,30559:SPIDER_DUST,30587:DEEP_SEA_ORB}
 
 #REWARDS
 CLUB     = 4
-ADENA_ID = 57
-
 
 class Quest (JQuest) :
 
- def __init__(self,id,name,descr):
-     JQuest.__init__(self,id,name,descr)
-     self.questItemIds = range(1541,1547)
+ def __init__(self,id,name,descr): JQuest.__init__(self,id,name,descr)
 
  def onEvent (self,event,st) :
     htmltext = event
     if event == "30578-03.htm" :
       st.set("cond","1")
       st.set("id","1")
-      st.setState(State.STARTED)
+      st.setState(STARTED)
       st.playSound("ItemSound.quest_accept")
     return htmltext
 
  def onTalk (self,npc,player):
-   htmltext = "<html><body>You are either not on a quest that involves this NPC, or you don't meet this NPC's minimum quest requirements.</body></html>"
+   htmltext = "You are either not on a quest that involves this NPC, or you don't meet this NPC's minimum quest requirements.</body></html>"
    st = player.getQuestState(qn)
    if not st : return htmltext
 
    npcId = npc.getNpcId()
    id = st.getState()
    cond = st.getInt("cond")
-   if id == State.COMPLETED :
+   if id == COMPLETED :
      htmltext = "<html><body>This quest has already been completed.</body></html>"
    elif npcId == NAKUSIN :
      if cond == 0 :
        if player.getRace().ordinal() != 3 :
          htmltext = "30578-00.htm"
          st.exitQuest(1)
-       elif player.getLevel() >= 2 and player.getLevel() <= 5 :
+       elif player.getLevel() >= 2 :
          htmltext = "30578-02.htm"
        else:
          htmltext = "30578-01.htm"
@@ -69,14 +59,12 @@ class Quest (JQuest) :
      elif cond == 2 :
        htmltext = "30578-06.htm"
        st.giveItems(CLUB,1)
-       st.rewardItems(ADENA_ID,1850)
        for item in NPC_GIFTS.values():
            st.takeItems(item,-1)
-       st.addExpAndSp(4254,335)
        st.unset("cond")
-       st.exitQuest(False)
+       st.setState(COMPLETED)
        st.playSound("ItemSound.quest_finish")
-   elif npcId in NPC_GIFTS.keys() and cond == 1 and id == State.STARTED:
+   elif npcId in NPC_GIFTS.keys() and cond == 1 and id == STARTED:
      item=NPC_GIFTS[npcId]
      npc=str(npcId)
      if st.getQuestItemsCount(item) :
@@ -96,7 +84,12 @@ class Quest (JQuest) :
    return htmltext
 
 QUEST     = Quest(4,qn,"Long Live the Paagrio Lord")
+CREATED   = State('Start',     QUEST)
+STARTING  = State('Starting',  QUEST)
+STARTED   = State('Started',   QUEST)
+COMPLETED = State('Completed', QUEST)
 
+QUEST.setInitialState(CREATED)
 QUEST.addStartNpc(NAKUSIN)
 
 QUEST.addTalkId(NAKUSIN)
